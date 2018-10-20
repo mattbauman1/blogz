8de5bcd5e2a6a1a31aa77997c6d7a75ae18c6dbe
Matt Bauman <mattbauman1@gmail.com>
2018-10-19 19:21:42 -0500

    Basic Blogz Functionality

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ from flask import Flask, request, redirect, render_template, session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from timestamp import Timestamp
-from sortposts import SortPosts
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:123@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:123@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = '12345'
@@ -16,35 +15,135 @@ class Post(db.Model):
     name = db.Column(db.String(60))
     content = db.Column(db.String(1000))
     timestamp = db.Column(db.DateTime)
+    deleted = db.Column(db.Boolean)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, name, content, timestamp):
+    def __init__(self, name, content, timestamp, owner):
         self.name = name
         self.content = content
         self.timestamp = timestamp
+        self.deleted = False
+        self.owner = owner
 
-    def __repr__(self):
-        return repr((self.timestamp))
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(30), unique = True)
+    password = db.Column(db.String(30))
+    posts = db.relationship('Post', backref = 'owner')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+def username_verification(username):
+    if username == "":
+        return '"Username" cannot be blank!'
+    elif re.match("^\s.", username) != None:
+        return "You cannot have a space at the beginning of your username!"
+    elif re.match("^.+\s+.", username) != None:
+        return "You cannot have a space anywhere in your username!"
+    elif re.match("^.+\s", username) != None:
+        return "You cannot have a space at the end of your username!"
+    elif re.match("^\^", username) or re.match("^.+\^", username) != None:
+        return 'You cannot have a "^" in your username!'
+    elif re.match("^[a-zA-Z0-9-_.]{3,20}$", username) == None:
+        return 'The "Username" must be between 3 and 20 characters in length and can have no special characters or spaces.'
+    else:
+        return ""
+
+def password_verification(password):
+    if password == "":
+        return '"Password" cannot be blank!'
+    elif re.match("^[a-zA-Z0-9-_.]{3,20}$", password) == None:
+        return 'The "Password" must be between 3 and 20 characters in length and can have no special characters or spaces'
+    else:
+        return ""
+
+def verifypassword_verification(password, verifypassword):
+    if verifypassword == "":
+        return 'You must retype your "Password" here!'
+    elif password != verifypassword:
+        return "The passwords you entered do not match, please try again!"
+    else:
+        return ""
+
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'blog', 'signup', 'index']
+    if request.endpoint not in allowed_routes and 'username' not in session:
+        return redirect('/login')
+
+@app.route('/logout')
+def logout():
+    del session['username']
+    return redirect('/blog')
+
+@app.route('/', methods=['GET'])
+def index():
+    users = User.query.all()
+    users.sort(key=lambda user: user.name)
+    return render_template('index.html', title="Blogz", users = users)
+
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    if  request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        verifypassword = request.form.get('verifypassword')
+        adjustable_username_error = username_verification(username)
+        adjustable_password_error = password_verification(password)
+        adjustable_verifypassword_error = verifypassword_verification(password, verifypassword)
+        if adjustable_username_error != "" or adjustable_password_error != "" or adjustable_verifypassword_error != "" or adjustable_email_error != "":
+            return render_template('signup.html', title = "Blogz", old_username_value = username, old_email_value = email, username_error = adjustable_username_error, password_error = adjustable_password_error, verifypassword_error = adjustable_verifypassword_error)
+        existing_user = User.query.filter_by(username=username).first()
+        if not existing_user:
+            new_user = User(username, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['username'] = username
+            return redirect('/newpost')
+        else:
+            return render_template("signup.html", title = "Blogz", username_error = "This username already exist, please choose a different username!")
+    return render_template('signup.html', title = "Blogz")
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            session['username'] = username
+            return redirect('/newpost')
+        else:
+            if not user:
+                return render_template('login.html', title = "Blogz", old_username = username, username_error = 'Username does not exist, please <a href="/signup">sign-up,</a> for one!')
+            else:
+                return render_template('login.html', title = "Blogz", old_username = username, password_error = 'You entered an incorrect password for this account, please try again!')
+    return render_template('login.html', title = "Blogz")
 
 @app.route('/blog', methods=['GET'])
 def blog():
+    user_id = request.args.get('user')
+    if user_id != None:
+        posts = User.query.filter_by(id=user_id).first().posts.all()
+        posts.sort(key=lambda post: post.timestamp, reverse=True)
     post_id = request.args.get('id')
     if post_id != None:
-        post = Post.query.filter_by(id=post_id).all()
+        post = Post.query.filter_by(id=post_id)
         timestamp = Timestamp(post[0].timestamp).timestampformatter()
         return render_template('blog.html', title=post[0].name, posts = post[0], timestamp = timestamp, individual_post = True)
     else:
         posts = Post.query.all()
-        #for post in range(len(posts)):
-            #posts[post].timestamp = Timestamp(posts[post].timestamp).totalstamp()
-        #posts = SortPosts(posts).sortpostsbytimestampnormal()
         posts.sort(key=lambda post: post.timestamp, reverse=True)
-        return render_template('blog.html', title="Build A Blog", posts = posts, individual_post = False)
+        return render_template('blog.html', title="Blogz", posts = posts, individual_post = False)
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def newpost():
     if request.method == 'POST':
         post_name = request.form.get('post_name')
         post_content = request.form.get('post_content')
+        owner = User.query.filter_by(username = session['username']).first()
         if post_name == "" or post_content == "" or len(post_name) > 60 or len(post_content) > 1000:
             title_error = ""
             content_error = ""
@@ -57,7 +156,7 @@ def newpost():
             if len(post_content) > 1000:
                 content_error = "Your body cannot be more than 1000 characters."
             return render_template('newpost.html', title="Add Blog Entry", title_error=title_error, content_error = content_error, old_name = post_name, old_post = post_content)
-        new_post = Post(post_name, post_content, datetime.utcnow())
+        new_post = Post(post_name, post_content, datetime.utcnow(), owner)
         db.session.add(new_post)
         db.session.commit()
         return redirect('/blog?id='+str(new_post.id))
